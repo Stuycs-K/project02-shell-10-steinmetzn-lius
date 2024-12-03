ecb8a7d93840eec32b2c8d9ba18639cf00acf86b
Sophie Liu <sliu50@stuy.edu>
2024-12-03 15:35:08 -0500

    started ; thing

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "shell.h"
 
 int main() {
@@ -6,9 +7,17 @@ int main() {
     print_prompt();
     char * args[50];
     char line[250];
+    char * token;
     fgets(line, sizeof(line), stdin);
-    parse_args(line, args);
-    execute(args[0], args);
+    char * input = line;
+    while (token = strsep(&input, ";")) {
+      printf("token: %s\n", token);
+      parse_args(token, args);
+      execute(args[0], args);
+    }
+
+    //parse_args(line, args);
+    //execute(args[0], args);
   }
 
   return 0;
