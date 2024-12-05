abee4d6d5a24a82c94eec25b089ae7fa0b61e2a0
somebody <81702427+sOm38Ody@users.noreply.github.com>
2024-12-05 17:12:38 -0500

    fixed redirection >

--- main.c
+++ main.c
@@ -3,94 +3,47 @@
 #include <string.h>
 #include "shell.h"
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "shell.h"
-
 int main() {
   while (1){
     print_prompt();
     char * args[50];
     char line[250];
-    char * token;
+    char * command;
     if (fgets(line, sizeof(line), stdin) == NULL) {
       exit(1); // ctrl d
     }
     char * input = line;
-    // while ((token = strsep(&input, ";")) != NULL) {
-    //   parse_args(token, args);
-    //   execute(args[0], args);
-    // }
-    char * input2 = input;
-    while ((token = strsep(&input, ";")) != NULL) {
-      char * token2 = token;
-      input2 = strsep(&token2, ">");
-      if (token2 == NULL || strcmp(token2,input2) == 0){
-        parse_args(token, args);
+    while ((command = strsep(&input, ";")) != NULL) {
+
+      char * command_right = command;
+      char * command_left = strsep(&command_right, ">");
+      if (command_right == NULL || strcmp(command_right,command_left) == 0){
+
+        //remove leading and trailing spaces from command
+        while (*command == ' ') command++;
+        char * end = command + strlen(command) - 1;
+        while (end > command && *end == ' ') end--;
+        *(end + 1) = '\0';
+
+        parse_args(command, args);
         execute(args[0], args);
       }
-      else{
-        printf("input2: 0000%s000\n", input2);
-        printf("token2: 0000%s000\n", token2);
-        sscanf(token2, " %s", token2);
-        int place = redirectOut(token2);
-        parse_args(input2, args);
-        //execute(args[0], args);
-        //redirectOutBack(place);
+      else {
+
+        //remove leading spaces from right part of command and trailing spaces from left part of command
+        while (*command_right == ' ') command_right++;
+        char * end = command_left + strlen(command_left) - 1;
+        while (end > command_left && *end == ' ') end--;
+        *(end + 1) = '\0';
+
+        sscanf(command_right, " %s", command_right);
+        int place = redirectOut(command_right);
+        parse_args(command_left, args);
+        execute(args[0], args);
+        redirectOutBack(place);
       }
     }
   }
 
   return 0;
 }
-
-/*
-int main() {
-  while (1){
-    print_prompt();
-    char * args[50];
-    char line[250];
-    char * token;
-    char * token2;
-    if (fgets(line, sizeof(line), stdin) == NULL) {
-      exit(1); // ctrl d
-    }
-    char * input = line;
-*/
-
-    /*
-    while ((token = strsep(&input, ";")) != NULL) {
-      char * command = token;
-      while ((token2 = strsep(&command, ">"))) {
-        token2[strlen(token2)-1] = '\0';
-        printf("token2: %s000\n", token2);
-        int place = redirectOut(command);
-        parse_args(token2, args);
-        execute(token2, args);
-        redirectOutBack(place);
-      }
-      //parse_args(token, args);
-      //execute(args[0], args);
-    }
-    */
-  //   char * input2 = input;
-  //   while ((token = strsep(&input, ";")) != NULL) {
-  //     char * token2 = token;
-  //     input2 = strsep(&token2, ">");
-  //     if (strcmp(token2,input2) != 0){
-  //       int place = redirectOut(token2);
-  //       execute(input2, args);
-  //       redirectOutBack(place);
-  //     }
-  //     else{
-  //       parse_args(token, args);
-  //       execute(args[0], args);
-  //     }
-  //   }
-  // }
-//}
-
-  //return 0;
-//}
