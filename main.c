fe6414cf020ccec457b662e83a9fa39ec0f9f59b
somebody <81702427+sOm38Ody@users.noreply.github.com>
2024-12-08 23:37:01 -0500

    reformatted some code

--- main.c
+++ main.c
@@ -17,10 +17,8 @@ void handle_pipe(char * command, char * side) {
   // remove leading spaces
   while (*command == ' ') command++;
 
-  char *command_copy = strdup(command);
-
   if (strcmp(side, "left") == 0) {
-    char *input_redirect_pos = strchr(command_copy, '<');
+    char *input_redirect_pos = strchr(command, '<');
     if (input_redirect_pos != NULL) {
       input_redirect = 1;
       *input_redirect_pos = '\0';
@@ -30,17 +28,18 @@ void handle_pipe(char * command, char * side) {
     output_file = "temp";
 
     // remove trailing spaces
-    char * end = command_copy + strlen(command_copy) - 1;
-    while (end > command_copy && *end == ' ') {
+    char * end = command + strlen(command) - 1;
+    while (end > command && *end == ' ') {
         *end = '\0';
         end--;
     }
 
-    parse_args(command_copy, args);
+    parse_args(command, args);
     execute(args, input_redirect, output_redirect, input_file, output_file);
   }
+  // side == "right"
   else {
-    char *output_redirect_pos = strchr(command_copy, '>');
+    char *output_redirect_pos = strchr(command, '>');
     if (output_redirect_pos != NULL) {
       output_redirect = 1;
       *output_redirect_pos = '\0';
@@ -50,13 +49,13 @@ void handle_pipe(char * command, char * side) {
     input_file = "temp";
 
     // remove trailing spaces
-    char * end = command_copy + strlen(command_copy) - 1;
-    while (end > command_copy && *end == ' ') {
+    char * end = command + strlen(command) - 1;
+    while (end > command && *end == ' ') {
         *end = '\0';
         end--;
     }
 
-    parse_args(command_copy, args);
+    parse_args(command, args);
     execute(args, input_redirect, output_redirect, input_file, output_file);
     remove("temp");
   }
@@ -68,44 +67,36 @@ int main() {
     char line[MAX_INPUT_SIZE];
     if (fgets(line, sizeof(line), stdin) == NULL) {
         printf("\n");
-        break; //ctrl d exit
+        break; //ctrl-d exit
     }
 
     // strips newline character if present
     line[strcspn(line, "\n")] = '\0';
 
     char * input = line;
-    char *commands[MAX_ARGS];
     char * command;
-    int command_count = 0;
     
     // split input by semicolons
     while ((command = strsep(&input, ";")) != NULL) {
-        commands[command_count++] = command;
-    }
-    
-    for (int i = 0; i < command_count; i++) {
-        // pipe
-        if (strchr(commands[i], '|')) {
-          char *cmd1 = strsep(&commands[i], "|");
-          handle_pipe(cmd1, "left");
-          handle_pipe(commands[i], "right");
-        }
-        else {
+      // pipe
+      if (strchr(command, '|')) {
+        char *cmd1 = strsep(&command, "|");
+        handle_pipe(cmd1, "left");
+        handle_pipe(command, "right");
+      }
+      else {
+        char *args[MAX_ARGS];
 
         // preparing to handle redirection
         int input_redirect = 0, output_redirect = 0;
         char *input_file = NULL, *output_file = NULL;
         
         // remove leading spaces
-        while (*commands[i] == ' ') commands[i]++;
-
-        char *command_copy = strdup(commands[i]);
-        char *args[MAX_ARGS];
+        while (*command == ' ') command++;
 
         // check for redirection symbols
-        char *input_redirect_pos = strchr(command_copy, '<');
-        char *output_redirect_pos = strchr(command_copy, '>');
+        char *input_redirect_pos = strchr(command, '<');
+        char *output_redirect_pos = strchr(command, '>');
         
         if (input_redirect_pos != NULL || output_redirect_pos != NULL) {
           if (input_redirect_pos != NULL) {
@@ -120,18 +111,18 @@ int main() {
             output_file = strtok(output_redirect_pos + 1, " \t");
           }
         }
+
         // remove trailing spaces
-        char * end = command_copy + strlen(command_copy) - 1;
-        while (end > command_copy && *end == ' ') {
+        char * end = command + strlen(command) - 1;
+        while (end > command && *end == ' ') {
             *end = '\0';
             end--;
         }
 
-        parse_args(command_copy, args);
+        parse_args(command, args);
         execute(args, input_redirect, output_redirect, input_file, output_file);
-        }
+      }
     }
-    
   }
 
   return 0;
